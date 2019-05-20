6b98f5ce8696bb69b26d6d3e71c2ce17ea1434fe
Vincent Robert <vincent.robert@nanego.com>
2019-05-20 16:14:00 +0200

    Init new plugin

---
+++ README.md
@@ -0,0 +1,4 @@
+Redmine Service Catalog plugin
+===================
+
+This Redmine plugin adds a service catalog and helps users to route their requests.
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_service_catalog do
+  name 'Redmine Service Catalog plugin'
+  author 'Vincent ROBERT'
+  description 'This Redmine plugin adds a service catalog and helps users to route their requests'
+  version '0.0.1'
+  url 'https://github.com/nanego/redmine_service_catalog'
+  author_url 'https://github.com/nanego'
+end
